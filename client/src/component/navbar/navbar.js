460336c41a223a792b1c0ceb137783589fc1c94b
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-03-27 10:33:01 -0300

    Leye inline styling

--- client/src/component/navbar/navbar.js
+++ client/src/component/navbar/navbar.js
@@ -11,46 +11,62 @@ const Navbar = () => {
     setNavIsOpen(!navIsOpen);
   };
   return (
-    <nav>
-      <div className={`options ${navIsOpen ? "max-h-80" : "max-h-0"}`}>
-        <div className='logo'>
-          <img src={cowrywise} alt='logo-cowrywise' />
+    <nav className='md:mx-5 my-2 relative md:static bg-blue-500 md:max-h-max md:bg-transparent md:flex justify-between items-center'>
+      <div
+        className={`relative md:static bg-blue-500 md:max-h-max md:bg-transparent md:flex justify-between items-center ${
+          navIsOpen ? "max-h-80" : "max-h-0"
+        }`}
+      >
+        <div className='flex justify-between items-center bg-white'>
+          <img className='w-48 h-14' src={cowrywise} alt='logo-cowrywise' />
           <div
-            className={`toggle-menu ${navIsOpen ? "open" : "close"}`}
+            className={`toggle-menu z-50 w-10 flex md:hidden flex-col justify-center items-center gap-1 cursor-pointer ${
+              navIsOpen ? "open" : "close"
+            }`}
             onClick={handleToggleNav}
           >
-            <div className='line line-1'></div>
-            <div className='line line-2'></div>
+            <div className='line'></div>
+            <div className='line'></div>
           </div>
         </div>
-        <ul className={`links ${navIsOpen ? "open" : "close"}`}>
-          <li>
+        <ul
+          className={`links fixed top-0 left-0 right-0 bg-blue-500 pl-4 md:pl-0 md:bg-transparent md:static md:flex justify-between items-center gap-8 text-blue-500 transition-all md:transition-none duration-1000 ${
+            navIsOpen ? "open" : "close"
+          }`}
+        >
+          <li className='pt-16 md:pt-0 mb-9 md:mb-0 text-xl md:text-[1rem] cursor-pointer font-bold md:font-normal text-white md:text-[#7287A7] flex md:justify-between items-center'>
             <span>Personal</span>
             <span>
               <MdArrowDropDown />
             </span>
           </li>
-          <li>
+          <li className='mb-9 md:mb-0 text-xl md:text-[1rem] cursor-pointer font-bold md:font-normal text-white md:text-[#7287A7] flex md:justify-between items-center'>
             <span>Business</span>
             <span>
               <MdArrowDropDown />
             </span>
           </li>
 
-          <li>
+          <li className='mb-9 md:mb-0 text-xl md:text-[1rem] cursor-pointer font-bold md:font-normal text-white md:text-[#7287A7] flex md:justify-between items-center'>
             <span>Developer</span>
             <span>
               <MdArrowDropDown />
             </span>
           </li>
 
-          <li>Learn</li>
+          <li className='mb-9 md:mb-0 text-xl md:text-[1rem] cursor-pointer font-bold md:font-normal text-white md:text-[#7287A7] flex md:justify-between items-center'>
+            Learn
+          </li>
         </ul>
       </div>
 
-      <div className='auth'>
-        <button>Log In</button>
-        <button> Sign Up For Free</button>
+      <div className='gap-8 hidden md:flex justify-between items-center'>
+        <button className='font-bold text-blue-500 block md:inline-block'>
+          Log In
+        </button>
+        <button className='bg-blue-600 font-bold drop-shadow-md hover:drop-shadow-lg text-white rounded-md px-4 py-2'>
+          Sign Up For Free
+        </button>
       </div>
     </nav>
   );
