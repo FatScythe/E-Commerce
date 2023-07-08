c4287d864c0414a303194accb495d0ce96008496
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-08 19:59:00 +0100

    Worked on the user dashboard, can't figure how to route in it

--- client/src/assets/icons/icon.js
+++ client/src/assets/icons/icon.js
@@ -416,3 +416,41 @@ export const ErrorIcon = () => {
     </svg>
   );
 };
+
+export const LogoutIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9'
+      />
+    </svg>
+  );
+};
+
+export const HamburgerIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={2.0}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+      />
+    </svg>
+  );
+};
--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -1,6 +1,10 @@
+import "./user.css";
+import { useState } from "react";
 // Hooks
 import useTitle from "../../hooks/useTitle";
 // Components
+import { CloseIcon, HamburgerIcon, LogoutIcon } from "../../assets/icons/icon";
+
 // import NotNav from "../../component/noNavHeader";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
@@ -8,39 +12,54 @@ import { Navigate } from "react-router-dom";
 const User = () => {
   useTitle("User");
   const { user } = useSelector((store) => store.user);
-  console.log(user);
+  const [openNav, setOpenNav] = useState(false);
+  // console.log(user);
   if (!user) {
     return <Navigate to='/' />;
   }
+
   return (
-    <section id='user' className='grid grid-cols-12'>
-      <aside className='col-span-3 h-screen bg-blue-500 '>
+    <section id='user' className='relative sm:grid grid-cols-12'>
+      <aside
+        className={`absolute sm:static transition-all duration-300  sm:col-span-3 h-screen bg-blue-500 py-4 ${
+          openNav ? "left-0 w-3/5" : "-left-full w-0"
+        }`}
+      >
         {/* logo Here maybe?? AYETI ADORN*/}
         <h4 className='title mb-10 flex flex-col md:flex-row justify-center items-center gap-5'>
           <img
-            className='h-24 w-24 border border-red-500 rounded-full'
+            className='h-24 w-24rounded-full'
             src='http://localhost:3000/img.png'
             alt='name'
           />
-          <p className='font-semibold text-white'>Abdullahi Fahm</p>
+          <p className='font-semibold text-white capitalize'>abdullahi fahm</p>
         </h4>
 
         <nav className='bg-transparent p-0'>
           <ul className='ml-4 w-full bg-transparent'>
-            <li className='w-full my-6 p-3 rounded-l-md  text-black font-bold   bg-white'>
+            <li className='w-full my-4 p-3 rounded-l-md  text-black font-bold   bg-white'>
               Home
             </li>
-            <li className='my-6'>Edit profile</li>
-            <li className='my-6'>Change Password</li>
-            <li className='my-6'>My Store</li>
-            <li className='my-6'>My Products</li>
-            <li className='my-6'>Orders</li>
+            <li className='my-4'>Edit profile</li>
+            <li className='my-4'>Change Password</li>
+            <li className='my-4'>My Store</li>
+            <li className='my-4'>My Products</li>
+            <li className='my-4'>Orders</li>
           </ul>
         </nav>
 
-        <button>Logout</button>
+        <button className='mt-3 bg-tomato text-white flex justify-center items-center my-0 mx-auto px-3 py-2 w-3/4 rounded-md'>
+          <span>logout</span> <LogoutIcon />
+        </button>
       </aside>
-      <main className='col-span-9 h-full'></main>
+      <main className='sm:col-span-9 h-screen'>
+        <button
+          className='sm:hidden bg-bkg text-secondary p-4 rounded-full m-3 float-right'
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? <CloseIcon /> : <HamburgerIcon />}
+        </button>
+      </main>
     </section>
   );
 };
