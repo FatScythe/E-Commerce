a1ebb55ce478c2b841bd3f32a46aea491b50c19f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 17:38:03 +0100

    Added social media to footer

--- client/src/component/footer/footer.js
+++ client/src/component/footer/footer.js
@@ -1,6 +1,12 @@
 import "./footer.css";
 import Accordian from "./accordian/accordian";
 import { accordianData } from "../../assets/data/accordianData";
+import {
+  FBIcon,
+  InstaIcon,
+  PintrestIcon,
+  TikTokIcon,
+} from "../../assets/icons/icon";
 
 const Footer = () => {
   return (
@@ -12,13 +18,35 @@ const Footer = () => {
         })}
       </div>
 
+      <div className='my-5 sm:text-center'>
+        <h2 className='text-base font-bold sm:text-lg uppercase'>
+          Follow us on our social media
+        </h2>
+        <p className='my-2'>
+          Exclusive offers, a heads up on new things, and sightings of
+          Ayeti-Adorn in the wild. #adornedByayeti
+        </p>
+        <div className='flex justify-start sm:justify-center items-center gap-6 my-6 transition-all duration-700'>
+          <a href='https:://'>
+            <InstaIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
+          </a>
+          <a href='https:://'>
+            <TikTokIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
+          </a>
+          <a href='https:://'>
+            <FBIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
+          </a>
+          <a href='https:://'>
+            <PintrestIcon className='w-7 h-7 hover:scale-150 transition-all duration-300 ease-in-out' />
+          </a>
+        </div>
+      </div>
+
       <div className='legal'>
         <div>site</div>
         <div>legal & privacy</div>
         <div>cookies</div>
       </div>
-
-      <div></div>
     </footer>
   );
 };
