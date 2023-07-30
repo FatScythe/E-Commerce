0a2b0b35636d36203a37fc1ba55f5bbaf069c68a
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 17:39:00 +0100

    Added advert to become a vendor

---
+++ client/src/component/navbar/Ad.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { ChevronRight } from "../../assets/icons/icon";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+const Ad = () => {
+  const [nav, setNav] = useState(false);
+  const { user } = useSelector((store) => store.user);
+
+  const changeNav = () => {
+    window.scrollY <= 150 ? setNav(true) : setNav(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+
+    return () => window.removeEventListener("scroll", changeNav);
+  }, []);
+
+  if (user && user.role !== "user") {
+    return <></>;
+  }
+  return (
+    <Link
+      to='/stores'
+      className={`fixed overflow-hidden flex justify-between items-center transition-all top-0 left-0 right-0 duration-500 bg-black sm:bg-primary w-full text-white ${
+        !nav ? "h-0 -top-40" : "h-8 sm:h-8 z-50"
+      }`}
+    >
+      <span className='basis-11/12 text-sm sm:text-normal sm:font-semibold text-center'>
+        Own a store, become a vendor and get up to 90% payback
+      </span>
+      <span className='basis-1/12 flex justify-end items-center'>
+        <ChevronRight />
+      </span>
+    </Link>
+  );
+};
+
+export default Ad;
--- client/src/component/navbar/navbar.js
+++ client/src/component/navbar/navbar.js
@@ -3,116 +3,108 @@ import { NavLink, Link } from "react-router-dom";
 import { SearchIcon, ShoppingBagIcon, UserIcon } from "../../assets/icons/icon";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import Ad from "./Ad";
 
 const Navbar = () => {
-  // const [navBg, setNavBg] = useState(false);
-
   const { user } = useSelector((state) => state.user);
   const [isNavOpen, setIsNavOpen] = useState(false);
 
-  // const changeNavBg = () => {
-  //   window.scrollY >= 100 ? setNavBg(true) : setNavBg(false);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", changeNavBg);
-
-  //   return () => window.removeEventListener("scroll", changeNavBg);
-  // }, []);
-
   return (
     <nav>
-      <div className='logo'>
-        {user && (
-          <div className='icons'>
-            <button title={user.name}>
-              <Link to='/user/settings'>
-                <UserIcon />
-              </Link>
-            </button>
-          </div>
-        )}
-        <h2>
-          <Link to='/'>
-            ayeti adorn<span>.</span>
-          </Link>
-        </h2>
-
-        <div className='flex justify-evenly items-center gap-2 md:hidden'>
-          <button>
-            <Link to='/search'>
-              <SearchIcon />
-            </Link>
-          </button>
-          <button>
-            <Link to='/cart'>
-              <ShoppingBagIcon />
-            </Link>
-          </button>
-          <div
-            className={`toggle-menu ${isNavOpen ? "open" : "close"}`}
-            onClick={() => setIsNavOpen(!isNavOpen)}
-          >
-            <div className='line mb-1'></div>
-            <div className='line'></div>
-          </div>
-        </div>
-      </div>
-      <div
-        className={`navlinks-container ${
-          isNavOpen ? "max-h-full overflow-hidden" : "max-h-0"
-        }`}
-      >
-        <ul className='navlinks' onClick={() => setIsNavOpen(!isNavOpen)}>
-          <NavLink to='/'>
-            <li>home</li>
-          </NavLink>
-
-          <NavLink to='/products'>
-            <li>products</li>
-          </NavLink>
-
-          <NavLink to='/about'>
-            <li>about</li>
-          </NavLink>
-
-          <NavLink to='/contact'>
-            <li>contact</li>
-          </NavLink>
-
-          {!user && (
-            <li className='auth'>
-              <Link to='/auth'>login / register</Link>
-            </li>
-          )}
-        </ul>
-        <div className='options'>
-          <button className='bag'>
-            <Link to='/cart'>
-              <ShoppingBagIcon />
-            </Link>
-          </button>
-
-          <button className='search'>
-            <Link to='/search'>
-              <SearchIcon />
-            </Link>
-          </button>
-
+      <Ad />
+      <div className='flex items-center flex-col md:flex-row md:justify-between'>
+        <div className='logo'>
           {user && (
-            <button className='user' title={user.name}>
-              <Link to='/user/settings'>
-                <UserIcon />
-              </Link>
-            </button>
-          )}
-          {!user && (
-            <div className='auth'>
-              <button>
-                <Link to='/auth'>sign-up</Link>
+            <div className='icons'>
+              <button title={user.name}>
+                <Link to='/user/settings'>
+                  <UserIcon />
+                </Link>
               </button>
             </div>
           )}
+          <h2>
+            <Link to='/'>
+              ayeti adorn<span>.</span>
+            </Link>
+          </h2>
+
+          <div className='flex justify-evenly items-center gap-2 md:hidden'>
+            <button>
+              <Link to='/search'>
+                <SearchIcon />
+              </Link>
+            </button>
+            <button>
+              <Link to='/cart'>
+                <ShoppingBagIcon />
+              </Link>
+            </button>
+            <div
+              className={`toggle-menu ${isNavOpen ? "open" : "close"}`}
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
+              <div className='line mb-1'></div>
+              <div className='line'></div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`navlinks-container ${
+            isNavOpen ? "max-h-full overflow-hidden" : "max-h-0"
+          }`}
+        >
+          <ul className='navlinks' onClick={() => setIsNavOpen(!isNavOpen)}>
+            <NavLink to='/'>
+              <li>home</li>
+            </NavLink>
+
+            <NavLink to='/products'>
+              <li>products</li>
+            </NavLink>
+
+            <NavLink to='/about'>
+              <li>about</li>
+            </NavLink>
+
+            <NavLink to='/contact'>
+              <li>contact</li>
+            </NavLink>
+
+            {!user && (
+              <li className='auth'>
+                <Link to='/auth'>login / register</Link>
+              </li>
+            )}
+          </ul>
+          <div className='options'>
+            <button className='bag'>
+              <Link to='/cart'>
+                <ShoppingBagIcon />
+              </Link>
+            </button>
+
+            <button className='search'>
+              <Link to='/search'>
+                <SearchIcon />
+              </Link>
+            </button>
+
+            {user && (
+              <button className='user' title={user.name}>
+                <Link to='/user/settings'>
+                  <UserIcon />
+                </Link>
+              </button>
+            )}
+            {!user && (
+              <div className='auth'>
+                <button>
+                  <Link to='/auth'>sign-up</Link>
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
