33a798040c16d1e54ada692132e5e1c5f30f065f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-01 18:12:45 +0100

    Changes to the nav header

--- client/src/pages/user/dashboard/myStore.js
+++ client/src/pages/user/dashboard/myStore.js
@@ -4,7 +4,14 @@ const MyStore = ({ user }) => {
   if (user.role === "user") {
     return <Navigate to='/' />;
   }
-  return <div>My store</div>;
+  return (
+    <section>
+      <h2 className='capitalize font-semibold text-xl sm:text-2xl mb-10'>
+        My Store
+      </h2>
+      My store
+    </section>
+  );
 };
 
 export default MyStore;
