513fc1db0b11933ab56115b2ab9c724c56c66853
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-01 18:13:35 +0100

    Changes to the nav header

--- client/src/pages/user/dashboard/settings.js
+++ client/src/pages/user/dashboard/settings.js
@@ -1,5 +1,12 @@
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <section>
+      <h2 className='capitalize font-semibold text-xl sm:text-2xl mb-10'>
+        Edit Profile
+      </h2>
+      Settings
+    </section>
+  );
 };
 
 export default Settings;
