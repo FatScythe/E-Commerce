3482c2fff9a81de78adf356031a6eafd7904d418
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-01 18:19:29 +0100

    Worked on the orders page

--- client/src/pages/user/dashboard/orders.js
+++ client/src/pages/user/dashboard/orders.js
@@ -1,5 +1,99 @@
+import { NavLink, Routes, Route, useNavigate, Link } from "react-router-dom";
+import "../user.css";
+import { useEffect } from "react";
+
 const Orders = ({ user }) => {
-  return <div>My Order</div>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/user/orders/purchased");
+  }, [user]);
+
+  return (
+    <section id='order' className='container'>
+      <h2 className='capitalize font-semibold text-xl sm:text-2xl mb-10'>
+        Orders
+      </h2>
+      <nav className=''>
+        <ul className='flex justify-center items-center gap-5 border border-transparent border-b-black'>
+          <NavLink to='/user/orders/purchased'>
+            <li>Purchased</li>
+          </NavLink>
+
+          <NavLink to='/user/orders/sold'>
+            <li>Sold</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <main className='h-screen'>
+        <Routes>
+          <Route path='purchased' element={<Purchased />} />
+          <Route path='sold' element={<Sold user={user} />} />
+        </Routes>
+      </main>
+    </section>
+  );
 };
 
 export default Orders;
+
+const Card = () => {
+  return (
+    <div className='flex my-2 justify-between items-center border border-transparent border-t-black border-b-black'>
+      <div className='flex justify-between items-center'>
+        <img src='/img.png' className='w-40 h-40 object-cover' />
+        <div className='flex flex-col justify-between items-start gap-4'>
+          <div>
+            <h2 className='font-bold'>Completed</h2>
+            <p className='font-semibold'>Order #4452426</p>
+          </div>
+
+          <h4 className='italic font-medium'>01.07.2023</h4>
+        </div>
+      </div>
+
+      <div className='flex flex-col justify-between items-center gap-3'>
+        <h2>Product name</h2>
+        <div className=''>
+          <p className='bg-red-500 w-5 h-5 rounded-full mb-2'></p>
+          <p>XL</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Purchased = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+const Sold = ({ user }) => {
+  if (user.role === "user") {
+    return (
+      <div className='text-center mt-10'>
+        <h2 className='text-lg sm:text-xl font-semibold'>Become a Vendor ?</h2>
+        <p className='text-base'>Get Up to 90% pay back on all items sold</p>
+        <Link
+          to=''
+          className='block mx-auto text-base my-5 bg-black text-white border-2 hover:border-black hover:bg-transparent hover:text-black rounded-3xl w-full sm:w-3/4 p-4 transition-all duration-500 ease-in-out'
+        >
+          Click here to get started
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div className='h-screen'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
