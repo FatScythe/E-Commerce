76a2c9f51ca66c1c7a76719145a4d9ed091cecb7
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-03 23:44:29 +0100

    Added logic for when user is a seller

--- client/src/pages/store/createStore.js
+++ client/src/pages/store/createStore.js
@@ -1,7 +1,26 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { ArrowUpRight } from "../../assets/icons/icon";
+// Component
+import StoreForm from "../../component/store";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+
 const CreateStore = () => {
+  const { user } = useSelector((store) => store.user);
+  const [value, setValue] = useState({
+    name: "",
+    desc: "",
+    insta: "",
+    fb: "",
+    tiktok: "",
+    type: "add",
+    loading: false,
+    show: true,
+    storeId: "",
+  });
+
+  if (user && user.role === "seller") {
+    return <Navigate to='/user/store' />;
+  }
   return (
     <section id='create-store' className='container mt-8'>
       <h2 className='text-lg sm:text-xl font-semibold sm:font-bold'>
@@ -13,99 +32,106 @@ const CreateStore = () => {
         product
       </p>
 
-      <StoreForm />
+      <StoreForm value={value} setValue={setValue} />
     </section>
   );
 };
 
 export default CreateStore;
 
-const StoreForm = () => {
-  const [value, setValue] = useState({
-    name: "",
-    desc: "",
-    insta: "",
-    fb: "",
-    tiktok: "",
-    type: "add",
-    loading: false,
-    show: true,
-  });
+// const StoreForm = ({ value, setValue }) => {
+//   const dispatch = useDispatch();
 
-  return (
-    <form onSubmit={(e) => e.preventDefault()} className='mt-5'>
-      <main className='grid grid-cols-12 gap-3'>
-        <div className='col-span-12 sm:col-span-6'>
-          <label className='block font-bold pb-2'>Store Name</label>
-          <input
-            type='text'
-            value={value.name}
-            placeholder='Enter your Store name'
-            onChange={(e) => setValue({ ...value, name: e.target.value })}
-            className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2 placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
-          />
-        </div>
+//   const handleSubmit = (e) => {
+//     setValue({ ...value, loading: true });
+//     e.preventDefault();
+//     const { name, desc } = value;
+//     if ((!name, !desc)) {
+//       toast.error("Please provide store name and description");
+//       setValue({ ...value, loading: false });
+//       return;
+//     }
+//     dispatch(storeCrud(value));
+//     setValue({ ...value, loading: false });
+//     toast.success("You will be logged out now for changes to take effect");
+//     // log seller out
+//   };
 
-        <div className='col-span-12 sm:col-span-6'>
-          <label className='block font-bold pb-2'>Instagram Link</label>
-          <input
-            type='text'
-            value={value.insta}
-            placeholder='Enter your store Instagram link'
-            onChange={(e) => setValue({ ...value, insta: e.target.value })}
-            className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
-          />
-        </div>
+//   return (
+//     <form onSubmit={handleSubmit} className='mt-5'>
+//       <main className='grid grid-cols-12 gap-3'>
+//         <div className='col-span-12 sm:col-span-6'>
+//           <label className='block font-bold pb-2'>Store Name</label>
+//           <input
+//             type='text'
+//             value={value.name}
+//             placeholder='Enter your Store name'
+//             onChange={(e) => setValue({ ...value, name: e.target.value })}
+//             className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2 placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
+//           />
+//         </div>
 
-        <div className='col-span-12 sm:col-span-6'>
-          <label className='block font-bold pb-2'>Facebook Link</label>
-          <input
-            type='text'
-            value={value.fb}
-            placeholder='Enter your store Facebook link'
-            onChange={(e) => setValue({ ...value, fb: e.target.value })}
-            className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
-          />
-        </div>
+//         <div className='col-span-12 sm:col-span-6'>
+//           <label className='block font-bold pb-2'>Instagram Link</label>
+//           <input
+//             type='text'
+//             value={value.insta}
+//             placeholder='Enter your store Instagram link'
+//             onChange={(e) => setValue({ ...value, insta: e.target.value })}
+//             className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
+//           />
+//         </div>
 
-        <div className='col-span-12 sm:col-span-6'>
-          <label className='block font-bold pb-2'>TikTok Link</label>
-          <input
-            type='text'
-            value={value.tiktok}
-            placeholder='Enter your store Tiktok link'
-            onChange={(e) => setValue({ ...value, tiktok: e.target.value })}
-            className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
-          />
-        </div>
+//         <div className='col-span-12 sm:col-span-6'>
+//           <label className='block font-bold pb-2'>Facebook Link</label>
+//           <input
+//             type='text'
+//             value={value.fb}
+//             placeholder='Enter your store Facebook link'
+//             onChange={(e) => setValue({ ...value, fb: e.target.value })}
+//             className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
+//           />
+//         </div>
 
-        <div className='col-span-12 row-start-3 row-end-5'>
-          <label className='block font-bold pb-2'>Description</label>
-          <textarea
-            placeholder='Describe your store and products'
-            rows='10'
-            value={value.desc}
-            onChange={(e) => setValue({ ...value, desc: e.target.value })}
-            className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2 resize-none'
-          ></textarea>
-        </div>
-      </main>
-      <div className='my-4 flex gap-3 mb-10 flex-col justify-center items-center'>
-        <button
-          type='submit'
-          className='w-full sm:w-1/2 bg-black hover:bg-black/80 px-6 py-2 text-white focus:bg-gray-600 focus:outline-none first-letter:uppercase'
-        >
-          create store
-        </button>
-        {value.show && (
-          <Link
-            to='user/products'
-            className='w-fit hover:text-blue-500 hover:border-b-blue-400 text-base flex justify-center items-center border border-transparent border-b-black pb-3'
-          >
-            My Products <ArrowUpRight />
-          </Link>
-        )}
-      </div>
-    </form>
-  );
-};
+//         <div className='col-span-12 sm:col-span-6'>
+//           <label className='block font-bold pb-2'>TikTok Link</label>
+//           <input
+//             type='text'
+//             value={value.tiktok}
+//             placeholder='Enter your store Tiktok link'
+//             onChange={(e) => setValue({ ...value, tiktok: e.target.value })}
+//             className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2'
+//           />
+//         </div>
+
+//         <div className='col-span-12 row-start-3 row-end-5'>
+//           <label className='block font-bold pb-2'>Description</label>
+//           <textarea
+//             placeholder='Describe your store and products'
+//             rows='10'
+//             value={value.desc}
+//             onChange={(e) => setValue({ ...value, desc: e.target.value })}
+//             className='bg-gray-300/30 w-full outline-none p-2 text-base placeholder:text-normal placeholder:text-slate-700 placeholder:pl-2 resize-none'
+//           ></textarea>
+//         </div>
+//       </main>
+//       <div className='my-4 flex gap-3 mb-10 flex-col justify-center items-center'>
+//         <button
+//           type='submit'
+//           disabled={value.loading}
+//           className='w-full sm:w-1/2 disabled:bg-gray-500 bg-black hover:bg-black/80 px-6 py-2 text-white focus:bg-gray-600 focus:outline-none first-letter:uppercase'
+//         >
+//           create store
+//         </button>
+//         {value.show && (
+//           <Link
+//             to='user/products'
+//             className='w-fit hover:text-blue-500 hover:border-b-blue-400 text-base flex justify-center items-center border border-transparent border-b-black pb-3'
+//           >
+//             My Products <ArrowUpRight />
+//           </Link>
+//         )}
+//       </div>
+//     </form>
+//   );
+// };
