f7912f3e0f70b4a158c00298b9aa993dbfc8c1e9
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-04 18:51:33 +0100

    Started work on Contact Page

--- client/src/pages/contact/index.js
+++ client/src/pages/contact/index.js
@@ -1,12 +1,24 @@
 // Hooks
 import useTitle from "../../hooks/useTitle";
+// Component
+import { WaveHandIcon } from "../../assets/icons/icon";
 
 const Contact = () => {
   useTitle("Contact Us");
 
   return (
     <section id='contact' className='container'>
-      <h3>Contact</h3>
+      <h2 className='text-md sm:text-2xl font-semibold'>
+        <span className='first-letter:uppercase block'>
+          love to hear from you,
+        </span>
+        <span className='flex justify-start items-center'>
+          <span className='first-letter:uppercase'>get in touch</span>
+          <span>
+            <WaveHandIcon className='w-10 h-10' />
+          </span>
+        </span>
+      </h2>
     </section>
   );
 };
