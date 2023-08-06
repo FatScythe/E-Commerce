63d19ba6e0cc25d50f193a54eefda1c98a7ff8d7
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-06 18:24:38 +0100

    Added no-refferer to a tags

--- client/src/component/footer/footer.js
+++ client/src/component/footer/footer.js
@@ -27,16 +27,32 @@ const Footer = () => {
           Ayeti-Adorn in the wild. #adornedByayeti
         </p>
         <div className='flex justify-start sm:justify-center items-center gap-6 my-6 transition-all duration-700'>
-          <a target='_blank' href='https://www.instagram.com/ayeti_adorn/'>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.instagram.com/ayeti_adorn/'
+          >
             <InstaIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
           </a>
-          <a target='_blank' href='https://www.instagram.com/ayeti_adorn/'>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.instagram.com/ayeti_adorn/'
+          >
             <TikTokIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
           </a>
-          <a target='_blank' href='https://www.instagram.com/ayeti_adorn/'>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.instagram.com/ayeti_adorn/'
+          >
             <FBIcon className='w-6 h-6 hover:scale-150 transition-all duration-300 ease-in-out' />
           </a>
-          <a target='_blank' href='https://www.instagram.com/ayeti_adorn/'>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.instagram.com/ayeti_adorn/'
+          >
             <PintrestIcon className='w-7 h-7 hover:scale-150 transition-all duration-300 ease-in-out' />
           </a>
         </div>
@@ -44,6 +60,7 @@ const Footer = () => {
 
       <a
         target='_blank'
+        rel='noreferrer'
         href='https://github.com/FatScythe/E-Commerce'
         className='legal'
       >
