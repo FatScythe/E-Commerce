ec79f0bbd8d91747c18c6bd47065ab3d6b2e138a
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-06 18:25:26 +0100

    Added timer icon

--- client/src/assets/icons/icon.js
+++ client/src/assets/icons/icon.js
@@ -744,3 +744,22 @@ export const WaveHandIcon = ({ className }) => {
     </svg>
   );
 };
+
+export const Clock = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+      />
+    </svg>
+  );
+};
