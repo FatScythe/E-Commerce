776d4d6680c1aaa00e13fe7742870ec652f0bed2
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-01 13:05:12 +0100

    Modified order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -12,41 +12,53 @@ const singleOrderItemSchema = new Schema({
   },
 });
 
-const orderSchema = new Schema({
-  tax: {
-    type: Number,
-    required: true,
+const orderSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please provide your name"],
+    },
+    apartment: {
+      type: String,
+    },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    phone: { type: Number },
+    postal: { type: Number },
+    note: { type: String },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    orderItems: [singleOrderItemSchema],
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "delivered", "canceled"],
+      default: "pending",
+    },
+    user: {
+      type: Schema.ObjectId,
+      ref: "User",
+      required: [true, "Provide a user"],
+    },
+    referenceId: {
+      type: String,
+    },
   },
-  shippingFee: {
-    type: Number,
-    required: true,
-  },
-  subtotal: {
-    type: Number,
-    required: true,
-  },
-  total: {
-    type: Number,
-    required: true,
-  },
-  orderItems: [singleOrderItemSchema],
-  status: {
-    type: String,
-    enum: ["pending", "failed", "paid", "delivered", "canceled"],
-    default: "pending",
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  clientSecret: {
-    type: String,
-    required: true,
-  },
-  paymentIntentId: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("Orders", orderSchema);
