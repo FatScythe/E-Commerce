d9b33d12cb5fc3fb2ff133960e361ef78aefbad3
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-08 18:33:04 +0100

    Completed stripe contoller

--- server/controller/stripeCtrl.js
+++ server/controller/stripeCtrl.js
@@ -1,42 +1,49 @@
+const { BadRequestError, NotFoundError } = require("../errors");
+const Order = require("../models/Order");
+
 const stripe = require("stripe")(process.env.STRIPE_SECRETKEY);
 
 const calculateOrderAmount = (items) => {
-  // compare on the database
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return items.total_amount + items.shipping_fee;
+  const { subTotal, shippingFee } = items.order; // From DB
+  if (subTotal + shippingFee !== items.total + items.shipping) {
+    throw new BadRequestError("Prices do not match");
+  }
+
+  return items.total + items.shipping;
 };
 
-const stripeController = async (req, res) => {
-  // const { purchase, total_amount, shipping_fee } = req.body;
+const stripeCtrl = {
+  createPaymentIntent: async (req, res) => {
+    let { total, shipping, id } = req.body;
+    if (!shipping) {
+      shipping = 0;
+    }
 
-  // const paymentIntents = await stripe.paymentIntents.create({
-  //   amount: calculateOrderAmount({ total_amount, shipping_fee }),
-  //   currency: "usd",
-  //   automatic_payment_methods: {
-  //     enabled: true,
-  //   },
-  // });
+    if (!total || !id) {
+      throw new BadRequestError("Please provide amount and order ID");
+    }
 
-  // res.json({ clientSecret: paymentIntents.client_secret });
+    const order = await Order.findOne({ _id: id });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 2000,
-        quantity: 1,
+    if (!order) {
+      throw new NotFoundError("No order with with id: " + id);
+    }
+
+    const paymentIntents = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount({ id, total, shipping, order }),
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
       },
-    ],
-    mode: "payment",
-    success_url: `http://localhost:3000/paystack/verify?success=true`,
-    cancel_url: `http://localhost:3000/paystack/verify?canceled=true`,
-  });
+    });
 
-  console.log(session);
+    console.log(paymentIntents);
 
-  res.redirect(303, session.url);
+    order.clientSecret = paymentIntents.client_secret;
+    order.save();
+
+    res.json({ clientSecret: paymentIntents.client_secret });
+  },
 };
 
-module.exports = stripeController;
+module.exports = stripeCtrl;
