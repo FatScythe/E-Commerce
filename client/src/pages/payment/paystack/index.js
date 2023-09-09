c41a72ddf843f466febcb0c64a128686d21d137d
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-09 16:37:27 +0100

    Refactored paystack verify payment page

---
+++ client/src/pages/payment/paystack/index.js
@@ -0,0 +1,33 @@
+import { useSearchParams } from "react-router-dom";
+// Hook
+import useSWR from "swr";
+// Utils
+import url from "../../../utils/url";
+// Componenets
+import Loader1 from "../../../component/loaders/loader1";
+import TransactionError from "../error";
+import TransactionSuccess from "../success";
+import Error1 from "../../../component/loaders/error";
+
+const PaystackVerification = () => {
+  const [queryParameters] = useSearchParams();
+  const ref = queryParameters.get("reference");
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, isLoading, error } = useSWR(
+    url + "/api/v1/payment/paystack/verifyPayment/" + ref,
+    fetcher
+  );
+  if (isLoading)
+    return (
+      <div>
+        <Loader1 />
+      </div>
+    );
+  if (error) return <Error1 />;
+
+  if (data && data.data.status !== "success") return <TransactionError />;
+
+  return <TransactionSuccess />;
+};
+
+export default PaystackVerification;
