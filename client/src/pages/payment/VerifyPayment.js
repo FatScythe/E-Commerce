b49719cafb0210f8dcde858e4c54b532cba182d6
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-10 22:11:23 +0100

    Added component to verify stripe payment

--- client/src/pages/payment/VerifyPayment.js
+++ client/src/pages/payment/VerifyPayment.js
@@ -2,18 +2,18 @@ import { useSearchParams } from "react-router-dom";
 // Components
 import PaystackVerification from "./paystack";
 import Error1 from "../../component/loaders/error";
+import StripeVerification from "./stripe/stripeVerification";
 
 const VerifyPayment = () => {
   const [queryParameters] = useSearchParams();
   const mode = queryParameters.get("mode");
-  console.log(mode);
 
   if (mode === "paystack") {
     return <PaystackVerification />;
   }
 
   if (mode === "stripe") {
-    return <div> Verify Stripe Payment</div>;
+    return <StripeVerification />;
   }
 
   if (mode === "flutter") {
