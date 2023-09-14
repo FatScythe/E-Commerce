3f822444e067b1979046129d911e1a308ef73afe
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-14 21:47:37 +0100

    Modified Error component

--- client/src/component/loaders/error.js
+++ client/src/component/loaders/error.js
@@ -1,9 +1,9 @@
 import { RefreshIcon } from "../../assets/icons/icon";
-const Error1 = () => {
+const Error1 = ({ msg }) => {
   return (
     <section id='error1' className='flex flex-col justify-center items-center'>
       <h3 className='text-2xl text-red-400'>Error</h3>
-      <p className='text-xl italic'>Something went wrong : )</p>
+      <p className='text-xl italic'>{msg ? msg : "Something went wrong"} : )</p>
       <button
         className='flex gap-2 w-fit items-center my-5 bg-black text-white border-2 hover:border-black hover:bg-transparent hover:text-black rounded-3xl p-4  transition-all duration-500 ease-in-out'
         onClick={() => window.location.reload(true)}
