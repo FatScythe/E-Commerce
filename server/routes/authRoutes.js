948b62de2e38055e083583513a4cb0e7de7a59f6
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-23 21:47:50 +0100

    Added rate limiter to registration

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const rateLimit = require("express-rate-limit");
 const {
   register,
   verifyEmail,
@@ -9,7 +9,15 @@ const {
   forgotPassword,
 } = require("../controller/authCtrl");
 
-router.post("/register", register);
+const registerLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15min
+  limit: 2,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "You can register again after in less than 15min",
+});
+
+router.post("/register", registerLimiter, register);
 router.post("/verify-email", verifyEmail);
 router.post("/login", login);
 router.get("/logout", logout);
