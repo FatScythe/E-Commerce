ebcc7fae5bb2fc2bc253167d055ee9132e5a9b76
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-09-24 22:35:14 +0100

    Fetched recent avatar for show me

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -20,7 +20,11 @@ const getSingleUser = async (req, res) => {
 };
 
 const showCurrentUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ user: req.user });
+  const user = await User.findOne({ _id: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ user: { ...req.user, avatar: user.avatar } });
 };
 
 const updateUser = async (req, res) => {
